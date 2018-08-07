32e319e525c1e5a3d9b14cfaf63bda4063229ba2
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 10:31:10 +0300

    I hate CS Too much

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -20,16 +20,34 @@ add_article("Biochemistry","the study of chemical processes within and relating 
 	
 
 def query_all_articles():
-	pass
+	Topics= session.query(
+		Knowledge).all()
+      
+	return Topics
+print(query_all_articles())
 
-def query_article_by_topic():
-	pass
 
-def delete_article_by_topic():
-	pass
+def query_article_by_topic(tamara):
+	Idk = session.query(
+		Knowledge).filter_by(Topic_name=tamara).first()
+	return Idk
+print(query_article_by_topic("x"))
 
-def delete_all_articles():
-	pass
 
-def edit_article_rating():
-	pass
+def delete_article_by_topic(IalsoDoNotKnow):
+	session.query(Knowledge).filter_by(Topic_name=IalsoDoNotKnow).delete()
+	session.commit()
+delete_article_by_topic("Biochemistry")
+
+def delete_all_articles(Topic_name):
+	session.query(Knowledge).delete()
+	session.commit()
+delete_all_articles("Knowledge")
+
+def edit_article_rating(article_title,updated_rating):
+	potato_object=session.query(Knowledge).filter_by(Topic_name=article_title).first()
+	potato_object.Rating=updated_rating
+	session.commit()
+
+edit_article_rating("Biochemistry",8)
+
